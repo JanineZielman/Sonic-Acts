1ae9823f600c19bd5b48322e0504b59462c37cb7
JanineZielman <janine.zielman@icloud.com>
2021-10-27 11:58:27 +0200

    adding first component

---
+++ .slicemachine/assets/slices/TextBlock/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TextBlock';
+
+export default {
+  title: 'slices/TextBlock'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_block","items":[{"title":[{"type":"heading1","text":"Unleash impactful initiatives","spans":[]}],"text":[{"type":"paragraph","text":"Nulla id magna nisi veniam voluptate mollit dolore officia pariatur laborum aute. Nulla voluptate in fugiat ipsum. Deserunt reprehenderit id esse laborum culpa ut eu magna.","spans":[]}]},{"title":[{"type":"heading1","text":"Drive plug-and-play relationships","spans":[]}],"text":[{"type":"paragraph","text":"Labore non eu labore adipisicing culpa labore sunt labore mollit.","spans":[]}]}],"primary":{},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
