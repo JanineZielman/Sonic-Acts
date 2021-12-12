0153cede76baa553b7994ba564a3ba2fc39d221f
JanineZielman <janine.zielman@icloud.com>
2021-12-12 19:58:55 +0100

    adding loading screen

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,33 @@ import "../styles/globals.scss";
 import "../styles/landing.scss";
 import "../styles/festival/globals.scss";
 import "../styles/festival/programme.scss";
+import "../styles/loading.scss"
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import Loading from "../components/Loading/index.js"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+  
+  useEffect(() => {
+    const handleStart = (url) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    };
+    const handleComplete = (url) => setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+  }, [router]);
+
+  return (
+    <>
+      <Loading loading={loading} />  
+      <Component {...pageProps} />
+    </>
+  );
 };
 
 export default MyApp;
