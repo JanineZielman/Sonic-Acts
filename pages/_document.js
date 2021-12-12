c6246131a7c8844120ec6aea23f962cbf267c165
JanineZielman <janine.zielman@icloud.com>
2021-12-12 20:38:43 +0100

    preloading fonts

--- pages/_document.js
+++ pages/_document.js
@@ -15,6 +15,17 @@ export default class extends Document {
       <Html>
          <Head>
           <link href="/fonts/style.css" rel="stylesheet"/>
+          <link rel="preload" as="font" href="/fonts/Sonic-grime-open-01VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/Sonic-grime-open-02VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/Sonic-grime-open-03VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/Sonic-Dirt-open-01VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/Sonic-Dirt-open-02VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/Sonic-Dirt-open-03VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
+
+          <link rel="preload" as="font" href="/fonts/SuisseIntl-Regular-WebM.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/SuisseIntl-RegularItalic-WebM.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/SuisseIntl-Medium-WebM.woff2" type="font/woff2" crossorigin="anonymous"></link>
+          <link rel="preload" as="font" href="/fonts/SuisseIntl-MediumItalic-WebM.woff2" type="font/woff2" crossorigin="anonymous"></link>
         </Head>
 
         <body>
