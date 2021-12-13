5c9492e6b9f0927f221a3d100a63382fffd0a2e5
JanineZielman <janine.zielman@icloud.com>
2021-12-13 18:58:04 +0100

    small adjustments

--- .slicemachine/assets/slices/Collapsible/index.stories.js
+++ .slicemachine/assets/slices/Collapsible/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"collapsible","items":[],"primary":{"headline":[{"type":"heading3","text":"Synthesize viral ROI","spans":[]}],"text":[{"type":"paragraph","text":"Qui ullamco cupidatat dolor labore deserunt. Officia nulla adipisicing anim minim sunt cupidatat amet eiusmod voluptate duis. Aliquip elit cupidatat officia nostrud ullamco sit esse officia consectetur adipisicing quis in.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"collapsible","items":[],"primary":{"headline":[{"type":"heading3","text":"Transform rich interfaces","spans":[]}],"text":[{"type":"paragraph","text":"Eu elit nisi eu id magna laboris officia dolor occaecat incididunt incididunt incididunt cupidatat eiusmod Lorem.","spans":[]}],"open":true},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
--- slices/Collapsible/index.js
+++ slices/Collapsible/index.js
@@ -5,7 +5,7 @@ import { RichText } from 'prismic-reactjs'
 const CollapsibleSection = ({ slice }) => (
   <div>
     {slice.primary.headline != "" ?
-      <Collapsible trigger={RichText.asText(slice.primary.headline)}>
+      <Collapsible trigger={RichText.asText(slice.primary.headline)} open={slice.primary.open ? true : false }>
         <RichText render={slice.primary.text}/>
       </Collapsible>
       : null
