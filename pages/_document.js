3f0bb84572bb716d63f9e06aa81935779a990d9c
JanineZielman <janine.zielman@icloud.com>
2021-12-15 16:32:22 +0100

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,12 @@ export default class extends Document {
     return (
       <Html>
          <Head>
+          <link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png"/>
+          <link rel="manifest" href="favicon/site.webmanifest"/>
+          <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#000000"/>
+
           <link href="/fonts/style.css" rel="stylesheet"/>
           <link rel="preload" as="font" href="/fonts/Sonic-grime-open-01VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
           <link rel="preload" as="font" href="/fonts/Sonic-grime-open-02VF.woff2" type="font/woff2" crossorigin="anonymous"></link>
--- pages/index.js
+++ pages/index.js
@@ -6,22 +6,23 @@ import { useGetStaticProps } from "next-slicezone/hooks";
 import resolver from "../sm-resolver.js";
 
 const Page = (props) => {
-  const landingRef = useRef();
-  const [pageHeight, setPageHeight] = useState(parseInt(1000));
-  useEffect(() => {
-    setPageHeight(parseInt(landingRef.current.clientHeight));
-  }, []);
-  console.log(pageHeight)
+  // const landingRef = useRef();
+  // const [pageHeight, setPageHeight] = useState(parseInt(1000));
+  // useEffect(() => {
+  //   setPageHeight(parseInt(landingRef.current.clientHeight));
+  // }, []);
+  // console.log(pageHeight)
   return(
-    <section className="landing" id="landing" ref={landingRef} style={{'--pageHeight': pageHeight}}>
+    // <section className="landing" id="landing" ref={landingRef} style={{'--pageHeight': pageHeight}}>
+    <section className="landing" id="landing">
       <SliceZone {...props} resolver={resolver} />
-      <div className="wrapper">
+      {/* <div className="wrapper">
         <div className="particle elem1 anim-delay1"></div>
         <div className="particle elem2 anim-delay5"></div>
         <div className="particle elem3 anim-delay3"></div>
         <div className="particle elem4 anim-delay8"></div>
         <div className="particle elem5 anim-delay9"></div>
-      </div>
+      </div> */}
     </section>
     
   );
